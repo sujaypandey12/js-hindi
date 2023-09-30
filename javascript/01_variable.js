0edf036fa0709e39e16382dfb2f5b161e0b300d5
sujaypandey12 <133794237+sujaypandey12@users.noreply.github.com>
2023-09-30 07:12:19 +0000

    container config done

---
+++ javascript/01_variable.js
@@ -0,0 +1,10 @@
+const accountName="SUJAY"
+let accountEmail="sujay.com"
+var accountNumber=123432
+account_City="Varanasi"
+console.table([accountName,accountEmail,accountNumber,account_City]);
+/*
+prefer do not use var as because issue in block and functional scope
+*/
+
+
