2650f9b04bcdd6f371d36d0badc70df7bbcc9448
sujaypandey12 <133794237+sujaypandey12@users.noreply.github.com>
2023-09-30 08:53:43 +0000

    container config done

---
+++ javascript/datatype.js
@@ -0,0 +1,19 @@
+/*we are different type of data type in javascript
+1-primitive
+number => 2 to power 53
+bigint
+string =>""
+boolean=> true , false
+null => standalone value
+undefined =>
+symbol => unique
+2.non primitive
+//object
+
+*/
+//use of type of//
+console.log(typeof "sujay");
+console.log(typeof 1);
+console.log(typeof false);
+console.log(typeof null);
+console.log(typeof undefined);
