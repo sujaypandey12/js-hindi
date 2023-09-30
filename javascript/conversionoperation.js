f010122b93482942545693775641bba34f6f103f
sujaypandey12 <133794237+sujaypandey12@users.noreply.github.com>
2023-09-30 09:27:07 +0000

    container config done

---
+++ javascript/conversionoperation.js
@@ -0,0 +1,22 @@
+let accountName=""
+// coverting into number//
+let accountNname=Number(accountName)
+/*{ 
+    notes of number
+    "sujay" -> Nan 
+    ""   -> 0
+}*/
+console.log( accountNname);
+//converting into boolean
+let accountBo= Boolean(accountName)
+console.log(accountBo);
+console.log(typeof accountBo)
+/*
+{
+  conversion into boolean
+  "" -> false
+  "shjhjahdb" -> true
+}
+// for this type you can convert each data type into another and check it status//
+*/
+
